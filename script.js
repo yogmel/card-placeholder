f68d729e9cfc86cc4e2a61010d53a66205a1a718
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-10-22 21:08:51 -0300

    Anime card placeholder

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#nomePkm');
 
-const pokedexCaixa = document.querySelector("#pokedexCaixa");
-const pokedexImg = document.querySelector("#pokedexImg");
-const pokedexTitle = document.querySelector("#pokedexTitle");
-const pokedexSubtitle = document.querySelector("#pokedexSubtitle");
+const cardCaixa = document.querySelector("#cardCaixa");
+const cardUrl = document.querySelector("#cardUrl");
+const cardImg = document.querySelector("#cardImg");
+const cardType = document.querySelector("#cardType");
+const cardEpisodes = document.querySelector("#cardEpisodes");
+const cardSynopsis = document.querySelector("#cardSynopsis");
 const mensagemErro = document.querySelector("#mensagemErro");
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const requestUrl = `https://pokeapi.co/api/v2/pokemon/${input.value}`;
+  const requestUrl = `https://api.jikan.moe/v3/search/anime?q=${input.value}`;
 
   fetch(requestUrl)
     .then((response) => response.json())
     .then((responseData) => {
       mensagemErro.textContent = '';
 
-      const { id, name, types } = responseData;
+      const data = responseData.results[0];
+      const { url, title, image_url, type, episodes, synopsis } = data;
 
-      const elementTypes = types.map((typeInfo) => typeInfo.type.name);
+      console.log('episodes', data);
 
-      fillPokemonCard({
-        imgUrl: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
-        title: name,
-        subtitle:  elementTypes.join(' | '),
-        type: elementTypes[0]
-      });
+      cardCaixa.classList.add('active');
 
-      pokedexCaixa.classList.add(elementTypes[0]);
+      cardUrl.setAttribute('href', url);
+      cardUrl.innerHTML = title;
+      cardImg.setAttribute('src', image_url);
+      cardType.textContent = type;
+      cardEpisodes.textContent = `| ${episodes} episodes`;
+      cardSynopsis.innerHTML = synopsis;
     })
     .catch(() => {
-      mensagemErro.textContent = "Pokemon não encontrado";
-      fillPokemonCard({});
+      mensagemErro.textContent = "Anime não encontrado";
     })
 
 });
 
-const fillPokemonCard = ({ imgUrl = '', title = '', subtitle = '' }, type = '') => {
-  pokedexImg.setAttribute('src', imgUrl);
-  pokedexTitle.textContent = title;
-  pokedexSubtitle.textContent = subtitle;
-  pokedexCaixa.className = type && `card ${type}`;
-}
 
